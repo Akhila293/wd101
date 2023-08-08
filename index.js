2190858acc124507585de6f45d644f9ce5214b1f
Akhila293 <116826626+Akhila293@users.noreply.github.com>
2023-08-08 22:54:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,72 +1,50 @@
-
- const form = document.getElementById('registrationForm');
-const tableBody = document.getElementById('userTableBody');
-
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const dob = document.getElementById('dob').value;
-    const acceptedTerms = document.getElementById('acceptedTerms').checked;
-    
-    // Validate age between 18 and 55
-    const today = new Date();
-    const dobDate = new Date(dob);
-    const age = today.getFullYear() - dobDate.getFullYear();
-    if (age < 18 || age > 55) {
-        alert("Date of Birth must be between 18 and 55 years old.");
-        return;
-    }
-    
-    // Add the data to the table
-    const newRow = document.createElement('tr');
-    newRow.innerHTML = `
-        <td>${name}</td>
-        <td>${email}</td>
-        <td>${password}</td>
-        <td>${dob}</td>
-        <td>${acceptedTerms ? 'Yes' : 'No'}</td>
-    `;
-    tableBody.appendChild(newRow);
-    
-    // Save data to localStorage
-    const userData = {
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("registration-form");
+    const tableBody = document.getElementById("user-table-body");
+  
+    // Load saved data from web storage
+    const savedUsers = JSON.parse(localStorage.getItem("users")) || [];
+    savedUsers.forEach(user => {
+      addUserToTable(user);
+    });
+  
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+  
+      const name = document.getElementById("name").value;
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+      const dob = document.getElementById("dob").value;
+      const terms = document.getElementById("terms").checked;
+  
+      // Save data to web storage
+      const userData = {
         name: name,
         email: email,
         password: password,
         dob: dob,
-        acceptedTerms: acceptedTerms
-    };
-    
-    let storedData = localStorage.getItem('userData');
-    if (!storedData) {
-        storedData = [];
-    } else {
-        storedData = JSON.parse(storedData);
-    }
-    
-    storedData.push(userData);
-    localStorage.setItem('userData', JSON.stringify(storedData));
-    
-    // Clear form fields
-    form.reset();
-});
-
-// Load saved data from localStorage
-const savedUserData = localStorage.getItem('userData');
-if (savedUserData) {
-    const userDataArray = JSON.parse(savedUserData);
-    userDataArray.forEach(userData => {
-        const newRow = document.createElement('tr');
-        newRow.innerHTML = `
-            <td>${userData.name}</td>
-            <td>${userData.email}</td>
-            <td>${userData.password}</td>
-            <td>${userData.dob}</td>
-            <td>${userData.acceptedTerms ? 'Yes' : 'No'}</td>
-        `;
-        tableBody.appendChild(newRow);
+        terms: terms,
+      };
+  
+      let users = JSON.parse(localStorage.getItem("users")) || [];
+      users.push(userData);
+      localStorage.setItem("users", JSON.stringify(users));
+  
+      // Add user data to the table
+      addUserToTable(userData);
+  
+      // Clear form fields
+      form.reset();
     });
-}
+  
+    // Function to add user data to the table
+    function addUserToTable(user) {
+      const newRow = tableBody.insertRow();
+      newRow.insertCell().textContent = user.name;
+      newRow.insertCell().textContent = user.email;
+      newRow.insertCell().textContent = user.password;
+      newRow.insertCell().textContent = user.dob;
+      newRow.insertCell().textContent = user.terms ? "Accepted" : "Not Accepted";
+    }
+  });
+  
